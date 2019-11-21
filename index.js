77fb312a2f0b2b84f4f869add5ba58708f94cd26
devJin <shiplight@korea.ac.kr>
2019-11-21 10:16:56 +0900

    seperated tracking file

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const fs = require('fs');
 const bencode = require('bencode');
-
-const dgram = require('dgram');
-const Buffer = require('buffer').Buffer;
-const urlParse = require('url').parse;
+const tracker = require('./tracker');
 
 const torrent = bencode.decode(fs.readFileSync('test.torrent'));
 
-const url = urlParse(torrent.announce.toString('utf8'));
-const socket = dgram.createSocket('udp4');
-const myMsg = Buffer.from('test','utf8');
-
-//will not get result because the message is not in format
-socket.send(myMsg, 0, myMsg.length, url.port, url.host, ()=>{});
-socket.on('message', msg=>{
-    console.log('message:',msg);
+tracker.getPeers(torrent, peers => {
+    console.log('list of peers', peers);
 })
