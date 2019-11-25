ace45928511f1d7539e5f3b2f4f94bb349ca380e
devJin <shiplight@korea.ac.kr>
2019-11-25 17:11:02 +0900

    added torrent-parser

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const bencode = require('bencode');
 const tracker = require('./src/tracker');
+const torrentParser = require('./torrent-parser');
 
-const torrent = bencode.decode(fs.readFileSync('test.torrent'));
+const torrent = torrentParser.open('puppy.torrent');
 
 tracker.getPeers(torrent, peers => {
     console.log('list of peers', peers);
