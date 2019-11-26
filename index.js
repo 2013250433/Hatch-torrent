fb1918bca237ac442cda783f9a727eb1dc557e82
devJin <shiplight@korea.ac.kr>
2019-11-26 16:38:19 +0900

    added infohash and size in torrent-parser

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const bencode = require('bencode');
 const tracker = require('./src/tracker');
-const torrentParser = require('./torrent-parser');
+const torrentParser = require('./src/torrent-parser');
 
-const torrent = torrentParser.open('puppy.torrent');
+const torrent = torrentParser.open('test.torrent');
 
 tracker.getPeers(torrent, peers => {
     console.log('list of peers', peers);
--- src/torrent-parser.js
+++ src/torrent-parser.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 const bencode = require('bencode');
+const bignum = require('bignum');
 
 module.exports.open = (filepath) => {
     return bencode.decode(fs.readFileSync(filepath));
 };
 
 module.exports.size = torrent => {
-
+    const size = torrent.info.files ?
+    torrent.info.files.map(file => file.length).reduce((a,b) => a+b) :
+    torrent.info.length;
 };
 
 module.exports.infoHash = torrent => {
-
+    const info = bencode.encode(torrent.info);
+    return crypto.createHash('sha1').update(info).digest();
 };
